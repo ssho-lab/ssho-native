46037fc1ce113ccdc07acdc1b311479a7d019ed2
Zinyon <pjy20450107@gmail.com>
2020-05-29 18:14:20 +0900

    데이터 불러오기 수정, 스와이프 완료시 저장 로직 구현

--- stores/swiper.js
+++ stores/swiper.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import { decorate, observable, action, Autobind, toJS } from "mobx";
+import {
+  decorate,
+  observable,
+  action,
+  Autobind,
+  toJS,
+  asyncAction,
+} from "mobx";
+
 import SwiperRepository from "../repositories/SwiperRepository";
+import CardModel from "../models/CardModel";
 
 const swiperRepository = new SwiperRepository();
 
@@ -8,37 +17,35 @@ class SwiperStore {
   constructor(root) {
     this.root = root;
   }
+
   @observable cards = [];
+  @observable swipedAllCards = false;
+  @observable swipeDirection = "";
+  @observable cardIndex = 0;
   @observable swipeList = [];
   @observable swipeLogs = {
     startTime: "",
-    swipeList: this.swipeList,
+    swipeList: [],
   };
 
   @action // api를 통해 itemList 가져오기
-  getCardList = () => {
-    swiperRepository
-      .getItemList()
-      .then((response) => {
-        // 10개만
-        this.cards = response.data.slice(0, 10);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    const result = toJS(this.cards);
-    return result;
-  };
+  async getCardList(params) {
+    const response = await swiperRepository.getItemList();
+    const data = response.data.slice(0, 10);
+    this.cards = data.map((card) => new CardModel(card));
+  }
 
   @action // swipeLog startTime 저장
+  // moment.js 사용
   setStartTime = () => {
     const startTime = new Date().toLocaleString();
     this.swipeLogs.startTime = startTime;
+    console.log("startTime is " + startTime);
   };
 
   @action // Swipe할때마다 로그 저장하기
-  addSwipeLog = (card, score) => {
-    const itemId = card.id;
+  addSwipeLog = (cardIndex, score) => {
+    const itemId = this.cards[cardIndex].id;
     const swipeTime = new Date().toLocaleString();
     const swipe = {
       userId: 1,
@@ -47,7 +54,9 @@ class SwiperStore {
       swipeTime: swipeTime,
     };
     //console.log(swipe);
-    this.swipeList.push(swipe);
+    //this.swipeList.push(swipe);
+    this.swipeLogs.swipeList.push(swipe);
+    console.log(this.swipeLogs);
   };
 
   @action // save likeList
