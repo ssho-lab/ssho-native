6d998298be8eee0be4fece340c8aa9b9d5d8a212
Yoojin Kim <yoojinkim@Yoojinui-MacBookPro.local>
2020-06-01 15:14:07 +0900

    eject 후 babel 설정

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,12 @@ module.exports = function (api) {
           legacy: true,
         },
       ],
+      [
+        "@babel/plugin-proposal-class-properties", 
+        { 
+          "loose": true
+        },
+      ]
     ],
   };
 };
